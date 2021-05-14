2f01f1e22452771e087cd6cf229a0d512871cf47
Tejas <rajtejas18@gmail.com>
2021-05-14 23:55:07 +0530

    Cp3 done with 2 Test cases

--- codewars.js
+++ codewars.js
@@ -33,6 +33,54 @@ makeNegative(1); // return -1
 makeNegative(-5); // return -5
 makeNegative(0); // return 0
 makeNegative(0.12); // return -0.12*/
-function makeNegative(num) {
- return num > 0 ? -num : num
+// function makeNegative(num) {
+//  return num > 0 ? -num : num
+// }
+
+//Cp3
+/*Check if a key is present in every segment of size k in an array*/
+/*Example
+arr[] = { 3, 5, 2, 4, 9, 3, 1, 7, 3, 11, 12, 3} 
+x = 3 
+k = 3 
+Output : Yes */
+function findxinkindowSize( arr, x, k, n)
+{
+	let i;
+	for (i = 0; i < n; i = i + k) {
+
+		// Search x in segment starting
+		// from index i.
+		let j;
+		for (j = 0; j < k; j++)
+			if (arr[i + j] == x)
+				break;
+
+		// If loop didn't break
+		if (j == k)
+		return false;
+	}
+
+	// If n is a multiple of k
+	if (i == n)
+	return true;
+
+	// Check in last segment if n
+	// is not multiple of k.
+	let j;
+	for (j=i-k; j<n; j++)
+	if (arr[j] == x)
+		break;
+	if (j == n)
+	return false;
+	
+	return true;
 }
+// Test 1
+// let arr = [ 21, 23, 56, 65, 34, 54, 76, 32, 23, 45, 21, 23, 25 ];
+// Test 2
+let arr = [ 3, 5, 2, 4, 9, 3, 1, 7, 3, 11, 12, 3];
+	let x = 3, k = 3;
+	let n = arr.length;
+    let ans = findxinkindowSize(arr, x, k, n) ? "Yes":"No";
+    console.log(ans);
